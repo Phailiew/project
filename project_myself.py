d1410d547ba39dd153ebe294ca80805782238c05
Suchawadee W <64011630@kmitl.ac.th>
2021-12-15 23:04:47 +0700

    new one new new

--- project_myself.py
+++ project_myself.py
@@ -2,7 +2,7 @@
 import discord
 import random
 import json
-from discord.colour import Color
+#from discord.colour import Color
 from discord.ext import commands
 from datetime import datetime, timedelta
 
@@ -26,16 +26,19 @@ CLUB = 9827
 HEART = 9829
 # chr(SPADE)
 
+#list of card
 olst_card = ['As', '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', 'Js', 'Qs', 'Ks', \
              'Ad', '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d', 'Jd', 'Qd', 'Kd', \
              'Ac', '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'Jc', 'Qc', 'Kc', \
              'Ah', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', 'Jh', 'Qh', 'Kh']
 
+#list of value of card
 olst_value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, \
               1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0]
 
 
 
+#create class
 class user_data:
     def __init__(self, name, uid, balance, last_time, win, lost, money_earn, money_lost):
         self.name = name
@@ -47,23 +50,25 @@ class user_data:
         self.money_earn = money_earn
         self.money_lost = money_lost
 
-
+#set prefix and remove default help command(The new one is embed)
 bot = commands.Bot(command_prefix="+", help_command=None)
 
 
 # message_lastseen = datetime.now()
 
+#send message in console when bot is online
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
-
+#help command 
 @bot.command()
 async def help(ctx):
-    # help,test,send
+    # help,play,credit,roulette,stat
     emBed = discord.Embed(title="All commands", description="Prefix: + \n How to use: type +COMMAND to use and enjoy",
                           color=0xf8b195)
     emBed.add_field(name="help", value="To get more information about a specific commands", inline=False)
+    emBed.add_field(name="howto", value="To know about the condition about this game", inline=False)
     emBed.add_field(name="play", value="To enjoy your bet", inline=False)
     emBed.add_field(name="credit", value="To show how much credit you have left", inline=False)
     emBed.add_field(name="roulette", value="To earn more credit \n - you can use it once a day -", inline=False)
@@ -74,15 +79,21 @@ async def help(ctx):
                      icon_url="https://media.istockphoto.com/vectors/suits-of-playing-cards-icon-vector-id911680094")
 
     await ctx.channel.send(embed=emBed)
-    # อันแรกคือ parameter อันที่สองคือบอกว่าเราจะส่ง embed
+    # อันแรกคือ parameter อันที่สองคือบอกว่าเราจะส่ง embed - from doc
 
-
+#play command 
 @bot.command()
+#+play __ <- amount of our bet
 async def play(ctx, *, par):
+    #open file for read only
     file = open("data.json", 'r')
+    #load the information
     data = json.load(file)
+    #close
     file.close()
+    #debug -> send in console
     print('play')
+    
     score_p = 0
     score_d = 0
     lst_card = olst_card.copy()
@@ -96,9 +107,11 @@ async def play(ctx, *, par):
         par = int(par)
     except:
         print('wrong input')
+        await ctx.channel.send("wrong input")
 
     user = ctx.message.author
     D_user = None
+    #check if user already use bot or not
     for i in range(len(data['users'])):
         if data['users'][i]['id'] == user.id:
             D_user = user_data(data['users'][i]['name'], data['users'][i]['id'], data['users'][i]['balance'],
@@ -114,6 +127,7 @@ async def play(ctx, *, par):
     lst_popp = []  # player
     lst_popd = []  # dealer
     s = ""
+    #random 2 cards
     for i in range(2):
         rand1 = random.randint(0, len(lst_card) - 1)
         lst_popp.append(lst_card.pop(rand1))
@@ -246,9 +260,10 @@ async def play(ctx, *, par):
         elif player == 3:
             D_user.balance += par * 5
             D_user.money_earn += par * 5
-        elif (player == 1 or player == 2) and lst_popp[0][-1] == lst_popp[1][-1]:
+        elif lst_popp[0][-1] == lst_popp[1][-1] and (player == 1 or player == 2):
             D_user.balance += par * 2
             D_user.money_earn += par * 2
+            print("player")
         else:
             D_user.balance += par
             D_user.money_earn += par
@@ -264,9 +279,10 @@ async def play(ctx, *, par):
         elif dealer == 3:
             D_user.balance -= par * 5
             D_user.money_lost += par * 5
-        elif (dealer == 1 or dealer == 2) and lst_popd[0][-1] == lst_popd[1][-1]:
+        elif lst_popd[0][-1] == lst_popd[1][-1] and (dealer == 1 or dealer == 2):
             D_user.balance -= par * 2
             D_user.money_lost += par * 2
+            print("dealer")
         else:
             D_user.balance -= par
             D_user.money_lost += par
@@ -278,11 +294,18 @@ async def play(ctx, *, par):
                 D_user.win += 1
                 D_user.balance += par
                 D_user.money_earn += par
-            else:
+            elif score_p < score_d:
                 display = 'You lost'
                 D_user.lost += 1
                 D_user.balance -= par
                 D_user.money_lost += par
+            elif score_p == score_d:
+                display = 'Draw'
+                D_user.lost += 0
+                D_user.win += 0
+                D_user.balance += 0
+                D_user.money_earn += 0
+                D_user.money_lost += 0
 
         elif player == dealer == 1 or player == dealer == 2:
             prioP = lst_popp[0][-1] == lst_popp[1][-1]
@@ -295,7 +318,7 @@ async def play(ctx, *, par):
 
             elif not prioP and prioD:
                 display = 'You lost'
-                D_user.lose += 1
+                D_user.lost += 1
                 D_user.balance -= par * 2
                 D_user.money_lost += par * 2
 
@@ -436,4 +459,4 @@ async def credit(ctx):
     await ctx.channel.send(f"You have " + str(D_user.balance) + " now")
 
 
-bot.run("OTA5NDIyOTM4NDYwMTM1NDU0.YZEEHA.33rruodrgt4m_sYMlPr33UpSTaU")
+bot.run("")
